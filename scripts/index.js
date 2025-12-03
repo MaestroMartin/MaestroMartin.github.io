2b8fbd8bfd4a2ce0c8159d99791eecec7061414c
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2025-12-03 20:56:13 +0100

    load videos from JSON and render video cards

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+
+
+
+async function loadVideos() {
+    try {
+        const response = await fetch('/data/videos.json');
+        if (!response.ok) {
+            throw new Error('HTTP error ' + response.status);
+        }
+    
+
+    const videos = await response.json();
+
+    const container = document.querySelector('.js-videos')
+
+    let html = '';
+
+    videos.forEach(video => {
+        html += `
+            <div class="video-card-info">
+                <div class="video-thumbnail">
+                    <iframe class="video" src="${video.link}" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
+                    </iframe>
+                </div>
+                <div class="video-content">
+                    <div class="video-header">
+                        <h3>${video.title}</h3>
+                        <p class="video-description">${video.description}</p>
+                    </div>
+                    <span class="video category">${video.tags}</span>
+                    <div class="video-meta">
+                        <span class="upload-date">Upload Date: </span>
+                        <span class="views">Views: </span>
+                        <span class="author">${video.author}</span>
+                        <span class="stars"> Stars there wanna be</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+    container.innerHTML = html;
+    }catch (error) {
+        console.error('Error loading videos:', error);
+    }
+}
+
+loadVideos();
